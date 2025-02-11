5ebd723adbabbc182ba7d677572146b304aaa2ee
aimandzhi <aigulmndzh@gmail.com>
2025-02-11 10:44:22 +0300

    add http handler layer

---
+++ internal/handler/http/v1/router.go
@@ -0,0 +1,39 @@
+package v1
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+
+	"family-flow-app/internal/service"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
+
+const api = "/api"
+
+func NewRouter(ctx context.Context, log *slog.Logger, route *chi.Mux, services *service.Services) {
+	route.Use(middleware.Logger)
+	route.Use(middleware.RequestID)
+	route.Use(middleware.Recoverer)
+	route.Use(middleware.URLFormat)
+	//route.Use(mwLogger.New(log))
+	route.Use(render.SetContentType(render.ContentTypeJSON))
+
+	route.Route(
+		api, func(r chi.Router) {
+			r.Get("/ping", Ping())
+			newUserRoutes(ctx, log, r, services.User)
+		},
+	)
+}
+
+func Ping() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte("ok"))
+
+	}
+}
