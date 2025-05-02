138108447a20c37e2926cc20f01ff96acdf9ff2b
aimandzhi <aigulmndzh@gmail.com>
2025-05-02 19:50:47 +0300

    add Notification struct definition in entity package

--- internal/entity/notification.go
+++ internal/entity/notification.go
@@ -1,1 +1,13 @@
 package entity
+
+import `time`
+
+type Notification struct {
+	ID        string    `db:"id" json:"id"`
+	UserID    string    `db:"user_id" json:"user_id"`
+	Title     string    `db:"title" json:"title"`
+	Body      string    `db:"body" json:"body"`
+	Data      string    `db:"data" json:"data"`
+	IsRead    bool      `db:"is_read" json:"is_read"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+}
