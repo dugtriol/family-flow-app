99165b5180fda5c63de1fa8f536e7078d827f5cb
aimandzhi <aigulmndzh@gmail.com>
2025-05-02 19:50:38 +0300

    add Firebase initialization function and service account key

--- pkg/firebase/firebase.go
+++ pkg/firebase/firebase.go
@@ -1,1 +1,22 @@
 package firebase
+
+import (
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go/v4"
+	"google.golang.org/api/option"
+)
+
+const (
+	serviceAccountKeyPath = "family-flow-app-4900a-firebase-adminsdk-fbsvc-9e2d528ca1.json"
+)
+
+func Init(ctx context.Context) *firebase.App {
+	opt := option.WithCredentialsFile(serviceAccountKeyPath)
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing Firebase app: %v", err)
+	}
+	return app
+}
